6047380120607665db79afee9850b55d02662154
msventurini <matheus.venturini@inf.ufrgs.br>
2022-09-25 03:55:07 -0300

    Added project initial template.

---
+++ movs/movs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movs
+//
+//  Created by Matheus Silveira Venturini on 25/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
