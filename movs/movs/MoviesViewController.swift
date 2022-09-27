77ddfd6ceaefde39a2047df37798c124bdbe52e9
msventurini <matheus.venturini@inf.ufrgs.br>
2022-09-27 18:27:59 -0300

    First try on adding StackView.

--- movs/movs/MoviesViewController.swift
+++ movs/movs/MoviesViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class MoviesViewController: UIViewController {
+    
+    
+    var stackView = UIStackView()
+    //var movieTitle = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupNavigation()
         self.setupTabBar()
         self.setupMainScreen()
+                
+        stackView.axis = .vertical
+        stackView.distribution = .equalCentering
+        stackView.spacing = 30
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(stackView)
+        
+        for _ in 0...30 {
+            let movieTitle = UILabel()
+            movieTitle.translatesAutoresizingMaskIntoConstraints = false
+            movieTitle.text = "Movie tittle placeholder"
+            movieTitle.font = UIFont.systemFont(ofSize: 24, weight: .medium)
+            movieTitle.textColor = UIColor(named: "FontColor")
+            movieTitle.textAlignment = .center
+            
+            stackView.addArrangedSubview(movieTitle)
+        }
+        
+        
     }
 
 }
