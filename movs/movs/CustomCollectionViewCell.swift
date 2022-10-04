ba6b054d08835955c4a47d79624f72a0b6ad4eb6
msventurini <matheus.venturini@inf.ufrgs.br>
2022-10-04 00:44:37 -0300

    Added App Icons in a proper way.

---
+++ movs/movs/CustomCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomCollectionViewCell.swift
+//  movs
+//
+//  Created by Matheus Silveira Venturini on 29/09/22.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier:String = "CustomCollectionViewCell"
+    
+    let customView:CustomView = {
+        let view = customView
+    }
+    
+    
+}
