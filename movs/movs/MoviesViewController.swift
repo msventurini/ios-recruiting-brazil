1a9082ba06d1ef45d263a9bc6daad866ed5406fb
msventurini <matheus.venturini@inf.ufrgs.br>
2022-10-07 04:20:50 -0300

    Solved the API Key problem.

--- movs/movs/MoviesViewController.swift
+++ movs/movs/MoviesViewController.swift
@@ -12,25 +12,35 @@ class MoviesViewController: UIViewController {
     
     var stackView = UIStackView()
     
+    var apiKey: String? {
+        return Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String
+    }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupNavigation()
         self.setupTabBar()
         self.setupMainScreen()
-                
+        
+        
         stackView.axis = .vertical
         stackView.distribution = .equalCentering
         stackView.spacing = 30
         stackView.translatesAutoresizingMaskIntoConstraints = false
         
+        
+        
+        
         view.addSubview(stackView)
+        
+        
         
         
         Task {
             
             do {
                 
-                let teste = try await MoviesViewController.fetchPopularMovies()
+                let teste = try await MoviesViewController.fetchPopularMovies(key: apiKey!)
                 //print(teste[0])
                 //updateCollectionViewSnapshot(albums)
                 
@@ -55,9 +65,11 @@ class MoviesViewController: UIViewController {
     }
     
     
-    static func fetchPopularMovies() async throws -> [Movie] {
+    static func fetchPopularMovies(key: String) async throws -> [Movie] {
         
-        guard let url = URL(string: ) else { throw ServiceError.invalidURL }
+        var urlPath: String = "https://api.themoviedb.org/3/movie/popular?api_key=\(key)&language=en-US&page=1"
+
+        guard let url = URL(string: urlPath) else { throw ServiceError.invalidURL }
         
         let (data, _) = try await URLSession.shared.data(from: url)
         
